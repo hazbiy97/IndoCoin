e426e3efef3c530bb021bb30027d7af14898b2b8
hazbiy97 <hazbiy97@users.noreply.github.com>
2018-06-29 18:33:39 +0700

    Initial Commit

---
+++ IndoCoin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IndoCoin
+//
+//  Created by Nur  Hazbiy on 29/06/18.
+//  Copyright © 2018 Nur  Hazbiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
